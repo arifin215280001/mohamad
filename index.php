5740ca9839cd714316e6c98b34be41aed6f1f26a
arifin215280001 <46618498+arifin215280001@users.noreply.github.com>
2019-01-21 23:00:41 -0800

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$nim = "215280001";
+$nama = "Mohamad Arifin";
+$kelas = "A";
+  echo "<table>
+         <tr>
+          <td>:NIM</td>;
+          <td>:$nim</td>;
+         </tr>
+         <tr>
+          <td>:NAMA</td>;
+          <td>:$nama</td>;
+         </tr>
+         <tr>
+          <td>:KELAS/td>;
+          <td>:$kelaS</td>;
+         </tr>
+        </table>
+          
